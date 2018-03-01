07a79b2c3fd37af11a913d0e1331d4204d2980da
Omar Ashraf <omar.mohamed1@emc.com>
2018-03-01 19:11:28 +0200

    changing backgrounds + redirection to register page

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'home',
@@ -7,7 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) {}
+
+  redirectToRegister(): void {
+    this.router.navigate(['./signup']);
+  }
+
+  redirectToLogin(): void {
+    
+  }
 
   ngOnInit() {
   }
