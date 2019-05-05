8395a8cb0a49862d84484d071b646170cc0db32f
nabegh <mahmoud.said1@emc.com>
2019-05-05 06:25:02 -0700

    add component to view all ideas

---
+++ src/app/view-all-ideas/view-all-ideas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-all-ideas',
+  templateUrl: './view-all-ideas.component.html',
+  styleUrls: ['./view-all-ideas.component.css']
+})
+export class ViewAllIdeasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
