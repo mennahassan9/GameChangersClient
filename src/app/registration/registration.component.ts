48f3f04bae11c1fd1705527504550ca9f49f1f04
nabegh <mahmoud.said1@emc.com>
2019-05-07 02:11:09 -0700

    remove other chapter onSelect method

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -15,7 +15,6 @@ import { HeaderButtonsService } from '../Services/headerButtons.service';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
-  otherChapterField: boolean;
   match: boolean;
   form: FormGroup;
   chapters: Array<String>;
@@ -162,17 +161,6 @@ export class RegistrationComponent implements OnInit {
   detectNewIdeaOrder(ideas) {
     ideas = ideas as IdeaModel;
     this.ideas = ideas;
-  }
-  onSelect(data) {
-    if (data == "Other") {
-      this.otherChapterField = true;
-      this.form.get('otherChapter').setValidators(Validators.required)
-    }
-    else {
-      this.otherChapterField = false;
-      this.form.get('otherChapter').setValue("");
-      this.form.get('otherChapter').clearValidators();
-    }
   }
 showAlert(message) {
   this.errorAlert = true;
