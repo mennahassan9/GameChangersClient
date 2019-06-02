2c9bd0c6f5e527c6d585128800c606a7e513b175
Mahmoud Nabegh <mahmoudnabegh@gmail.com>
2019-06-02 15:21:39 +0200

    added success alert and cleared form upon submit

--- src/app/admin/invite-leader/invite-leader.component.ts
+++ src/app/admin/invite-leader/invite-leader.component.ts
@@ -23,71 +23,75 @@ export class InviteLeaderComponent implements OnInit {
   errorMessage: String;
   Cleader: boolean;
   Rleader: boolean;
+  doneAlert: boolean;
 
-  constructor(private fb: FormBuilder, private userSvc: UserService, private router: Router, private loginService: LoginService, 
+  constructor(private fb: FormBuilder, private userSvc: UserService, private router: Router, private loginService: LoginService,
     private headerButtonsService: HeaderButtonsService, private adminService: AdminService) { }
 
   ngOnInit() {
+    this.doneAlert = false
     this.chapters = new Array<any>()
     this.chapter = new Array<any>()
     this.ages = new Array<String>();
-   
+
     this.regions = new Array<String>();
-   
-    this.userSvc.getRegions().subscribe((res)=>
-  {
-    console.log("REGIONS",res)
-    res.data.forEach(element => {
-      this.regions.push(element.name)
-      
-    });
-    console.log(this.regions)
-  })
-  this.userSvc.getChapters().subscribe((res)=>
-  {
-    console.log("CHAPTERS",res)
-    this.chapter= res.data
-    console.log(this.chapters)
-  })
-   
-     
+
+    this.userSvc.getRegions().subscribe((res) => {
+      console.log("REGIONS", res)
+      res.data.forEach(element => {
+        this.regions.push(element.name)
+
+      });
+      console.log(this.regions)
+    })
+    this.userSvc.getChapters().subscribe((res) => {
+      console.log("CHAPTERS", res)
+      this.chapter = res.data
+      console.log(this.chapters)
+    })
+
+
     this.form = new FormGroup({
       name: new FormControl(''),
       email: new FormControl(''),
       region: new FormControl(''),
-        chapter: new FormControl(''),
-  
+      chapter: new FormControl(''),
+
     });
   }
   register() {
     this.submit = true;
-  
+
     if (this.form.valid) {
       // this.resortIdeas();
-      console.log(this.form.value,"VALUE")
-      if(this.Cleader==true){
-    this.adminService.inviteCleader(this.form.value)}
-    if(this.Rleader==true){
-    this.adminService.inviteRleader(this.form.value)}
+      console.log(this.form.value, "VALUE")
+      if (this.Cleader == true) {
+        this.adminService.inviteCleader(this.form.value)
+      }
+      if (this.Rleader == true) {
+        this.adminService.inviteRleader(this.form.value)
+      }
+      this.doneAlert = true;
+      this.form.reset()
+      this.submit = false
     }
   }
- 
- 
 
- 
- 
-showAlert(message) {
-  this.errorAlert = true;
-  this.errorMessage = message;
-}
 
-getChapters(){
-  this.chapters=[]
-  console.log(this.form.get('region').value)
- this.chapter.forEach(chapter=> {
-   if(chapter.region.name == this.form.get('region').value)
-  {this.chapters.push(chapter.name)}
-})
 
-}
+
+
+  showAlert(message) {
+    this.errorAlert = true;
+    this.errorMessage = message;
+  }
+
+  getChapters() {
+    this.chapters = []
+    console.log(this.form.get('region').value)
+    this.chapter.forEach(chapter => {
+      if (chapter.region.name == this.form.get('region').value) { this.chapters.push(chapter.name) }
+    })
+
+  }
 }
