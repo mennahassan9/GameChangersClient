b0839413e28138b3b6dae1cae1bd6500a92d884d
mennahassan <mennawael7993@gmail.com>
2019-06-10 18:30:27 +0200

    Update admin-view-teams.component.ts

--- src/app/admin/admin-view-teams/admin-view-teams.component.ts
+++ src/app/admin/admin-view-teams/admin-view-teams.component.ts
@@ -53,7 +53,8 @@ export class AdminViewTeamsComponent implements OnInit {
     let output = [];
     input.forEach(element => {
       let object = {};
-      object['team name'] = element.name == undefined ? "" : `<a href="#/viewTeam/${element.name}">${element.name}</a>`;
+      var teamname=encodeURIComponent('#/viewTeam/'+element.name)
+     object['team name'] = element.name == undefined ? "" : `<a href='${teamname}'>${element.name}</a>`;
       object['members'] = element.members == undefined ? "" : element.members.map((member) => `<a href="/#/admin/user?user=${member.email}">${member.email}</a><br>`).join("");
       object['creator'] = element.creator == undefined ? "" : `<a href="/#/admin/user?user=${element.creator.email}">${element.creator.name}</a>`;
       object['region'] = element.region==undefined ? "" : element.region;
